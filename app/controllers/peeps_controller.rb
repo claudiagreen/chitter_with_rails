4416097f4640ac281a39142bb565a302e7af9840
Claudia Green <claudiagreen@Claudias-MacBook-Pro-2.local>
2016-05-02 16:53:20 +0100

    3 tests passing. Can create new Peep.

--- app/controllers/peeps_controller.rb
+++ app/controllers/peeps_controller.rb
@@ -1,6 +1,20 @@
 class PeepsController < ApplicationController
 
   def index
-    @peeps = Peeps.all
+    @peeps = Peep.all
   end
+
+  def new
+
+  end
+
+  def create
+    Peep.create(peep_params)
+    redirect_to '/peeps'
+  end
+
+  def peep_params
+    params.require(:peep).permit(:message)
+  end
+
 end
--- spec/features/chitter_feature_spec.rb
+++ spec/features/chitter_feature_spec.rb
@@ -6,7 +6,7 @@ feature 'peeps' do
     scenario 'should display a prompt to add a peep' do
       visit '/peeps'
       expect(page).to have_content 'No peeps yet'
-      expect(page).to have_link 'Add a peep'
+      expect(page).to have_link 'Write peep'
     end
   end
 
@@ -21,4 +21,17 @@ feature 'peeps' do
       expect(page).not_to have_content("No peeps yet")
     end
   end
+
+  context 'creating peeps' do
+    scenario 'prompts user to fill out form, then displays new peep' do
+      visit '/peeps'
+      click_link 'Write peep'
+      fill_in 'Message', with: 'Hello this is a peep'
+      click_button 'Create Peep'
+      expect(page).to have_content 'Hello this is a peep'
+      expect(current_path).to eq '/peeps'
+    end
+  end
+
+
 end
